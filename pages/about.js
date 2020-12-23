3e4fd799f7aeb4d63bdc6c5ee97dbced5a9880aa
Rohan <rohans540@gmail.com>
2020-12-24 00:24:00 +0530

    Setup React Next SSR App bitPrice

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+const About = () => (
+    <div>
+        <h1>About BitPrice</h1>
+        <p>Your one stop destination to view Bitcoin prices</p>
+    </div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => 
+(
+<div>
+    <h1>Welcome to NextJs App</h1>
+</div>
+);
+
+export default Index;
