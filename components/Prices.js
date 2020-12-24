7140696e44eda43a42b1d622f42c545451a8baed
Rohan <rohans540@gmail.com>
2020-12-24 16:45:28 +0530

    Allowing user to select the currency of his choice to check the price

--- components/Prices.js
+++ components/Prices.js
@@ -2,20 +2,44 @@ import React from 'react';
 
 class Prices extends React.Component {
     state = {
-        currency: 'USD'
+        currency: 'EUR'
     };
 
     render() {
 
+        let list = '';
+
+        switch(this.state.currency) {
+            case "USD":
+                list = "USD";
+                break;
+            case "GBP":
+                list = "GBP";
+                break;
+            case "EUR":
+                list = "EUR";
+                break;
+            default:
+                list = null;
+        };
+
+        let priceContent = (<li className="list-group-item">
+        Bitcoin Rate for {this.props.bpi.bpi[list].description}
+        : <span class="badge badge-primary">{this.props.bpi.bpi[list].code}</span> 
+        <strong>{this.props.bpi.bpi[list].rate}</strong>
+    </li>)
+
         return (
             <div>
                 <ul className="list-group">
-                    <li className="list-group-item">
-                        Bitcoin Rate for {this.props.bpi.bpi.USD.description}
-                        : <span class="badge badge-primary">{this.props.bpi.bpi.USD.code}</span> 
-                        <strong>{this.props.bpi.bpi.USD.rate}</strong>
-                    </li>
+                    {priceContent}
                 </ul>
+                <br />
+                <select onChange={e => this.setState({ currency: e.target.value })} className="form-control">
+                    <option value="USD">USD</option>
+                    <option value="GBP">GBP</option>
+                    <option value="EUR">EUR</option>
+                </select>
             </div>
         )
     }
--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,11 @@ const About = () => (
         <div>
             <h1>About BitPrice</h1>
             <p>Your one stop destination to view Bitcoin prices</p>
+            <p>This is a Demo React Next Application which uses React and NextJS libraries to render the pages at server side</p>
+            <p>Usually React Apps are client side applications the source code executes at client machine 
+                but there are some scenarios where
+                rendering pages at server gives us some power to boost our app speed and enhance the UX
+            </p>
         </div>
     </Layout>
 );
